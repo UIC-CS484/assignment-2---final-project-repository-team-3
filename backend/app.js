1d7ef3be25a7a6725ea2742549973ba6f89d2459
Nikhil Prasad <nprasad493@gmail.com>
2021-11-16 18:24:09 -0600

    Adding pokemon to favorites functionality

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,13 @@ const SQLiteStore = require('connect-sqlite3')(session);
 let passport = require("passport");
 
 const app = express();
+const port = 3000;
 
 require('./config/passport')(passport);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
 
 app.use(session({
     store: new SQLiteStore({

558a03cc7888fe051cd18727e15dbacf427706cb
Dhyan Patel <dhyanpp@gmail.com>
2021-12-07 18:43:53 -0600

    Password change fix

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.post('/updatePassword', (req, res, next) => {
     let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
 
     if (strongPassword.test(newPassword)) {
-        db.run('UPDATE users SET password = ? WHERE email = ?', [email, md5(newPassword)], (err, result) => {
+        db.run(`UPDATE users SET password = ? WHERE email = ?`, [md5(newPassword), email], (err, result) => {
             if (err) {
                 res.status(400).json({"error": err.message})
             } else {
@@ -33,7 +33,7 @@ router.post('/updatePassword', (req, res, next) => {
 
 router.post('/account', (req, res, next) => {
     const email = req.body.email;
-    db.run('DELETE FROM users WHERE email = ?;', [email], (err, result) => {
+    db.run(`DELETE FROM users WHERE email = ?;`, [email], (err, result) => {
         if (err) {
             res.status(400).json({"error": err.message})
         } else {
